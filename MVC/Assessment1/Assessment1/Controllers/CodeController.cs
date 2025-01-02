9fa43d55425cb87a06f11ead5818901f4ce415b9
Shreepriya Balamurugan <shreepriyab@infinite.com>
2025-01-02 19:30:36 +0530

    Assessment1 MVC

---
+++ MVC/Assessment1/Assessment1/Controllers/CodeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Assessment1.Models;
+
+namespace Assessment1.Controllers
+{
+    public class CodeController : Controller
+    {
+        // GET: Code
+        NorthwindEntities nw = new NorthwindEntities();
+        public ActionResult Index()
+        {
+            return View(nw.Customers.ToList());
+        }
+        public ActionResult In_Germany()
+        {
+            var customersInGermany = nw.Customers
+                                 .Where(c => c.Country == "Germany")
+                                 .ToList();
+            return View(customersInGermany);
+        }
+        public ActionResult CustomersByOrderId(int orderId = 10248)
+        {
+            var cust = nw.Customers.Where(c => c.Orders.Any(o => o.OrderID == orderId));
+            if (cust == null)
+            {
+                return HttpNotFound();
+            }
+            return View(cust);
+        }
+       
+    }
+}
