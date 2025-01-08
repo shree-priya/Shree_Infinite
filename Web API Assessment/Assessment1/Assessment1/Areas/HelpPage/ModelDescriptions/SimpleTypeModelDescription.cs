3e63ec9d4cb90834019dfe218fce9b9714a85bad
Shreepriya Balamurugan <shreepriyab@infinite.com>
2025-01-08 11:36:14 +0530

    Assessment1

---
+++ Web API Assessment/Assessment1/Assessment1/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace Assessment1.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
---
+++ Web API Assessment/Assessment1/Assessment1/Models/CountryController.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Assessment1.Models;
+
+
+namespace Assessment1.Controllers
+{
+    public class CountryController : ApiController
+    {
+        private static List<Country> countries = new List<Country>
+        {
+            new Country { ID = 1, CountryName = "India", Capital = "New Delhi" },
+            new Country { ID = 2, CountryName = "UK", Capital = "America" },
+            new Country { ID = 3, CountryName = "Japan", Capital = "Tokyo" }
+        };
+
+       
+        [HttpGet]
+        public IEnumerable<Country> GetAllCountries()
+        {
+            return countries;
+        }
+
+        // GET: api/Country/1
+        [HttpGet]
+        public IHttpActionResult GetCountryById(int id)
+        {
+            var country = countries.FirstOrDefault(c => c.ID == id);
+            if (country == null)
+                return NotFound();
+            return Ok(country);
+        }
+
+        // POST: api/Country
+        [HttpPost]
+        public IHttpActionResult AddCountry(Country country)
+        {
+            if (country == null)
+                return BadRequest("Invalid data.");
+
+            countries.Add(country);
+            return Ok(country);
+        }
+
+        // PUT: api/Country/1
+        [HttpPut]
+        public IHttpActionResult UpdateCountry(int id, Country updatedCountry)
+        {
+            var country = countries.FirstOrDefault(c => c.ID == id);
+            if (country == null)
+                return NotFound();
+
+            country.CountryName = updatedCountry.CountryName;
+            country.Capital = updatedCountry.Capital;
+            return Ok(country);
+        }
+
+        // DELETE: api/Country/1
+        [HttpDelete]
+        public IHttpActionResult DeleteCountry(int id)
+        {
+            var country = countries.FirstOrDefault(c => c.ID == id);
+            if (country == null)
+                return NotFound();
+
+            countries.Remove(country);
+            return Ok("Country deleted successfully.");
+        }
+
+    }
+}
